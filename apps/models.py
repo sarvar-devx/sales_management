07b8863313b4231d9a6056598320a0b4715a325b
sarvar-devx <sarvarbekdavranbekov@gmail.com>
2025-12-30 16:42:17 +0500

    new model

--- apps/models.py
+++ apps/models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator as username_validator
 from django.db.models import Model, ForeignKey, CASCADE
-from django.db.models.fields import CharField, DateTimeField, DecimalField, PositiveIntegerField
+from django.db.models.fields import CharField, DateTimeField, PositiveIntegerField, EmailField
 
 
 class TimeBaseModel(Model):
@@ -10,15 +12,32 @@ class TimeBaseModel(Model):
         abstract = True
 
 
+class User(AbstractUser):
+    username = CharField(
+        verbose_name="Login",
+        max_length=150,
+        unique=True,
+        help_text="Обязательно. Не более 150 символов. Только буквы, цифры и символы @/./+/-/_.",
+        validators=[username_validator()],
+        error_messages={
+            "unique": "Пользователь с таким именем пользователя уже существует.",
+        },
+    )
+    email = EmailField(
+        blank=True,
+        null=True,
+    )
+
+
 class Category(Model):
     name = CharField(max_length=255)
 
 
 class Product(TimeBaseModel):
     name = CharField(max_length=255)
-    arrival_price = DecimalField()
-    sales_price = DecimalField()
-    kaspi_price = DecimalField()
+    arrival_price = PositiveIntegerField()
+    sales_price = PositiveIntegerField()
+    kaspi_price = PositiveIntegerField()
     category = ForeignKey("apps.Category", CASCADE)
 
 
@@ -26,3 +45,4 @@ class Order(TimeBaseModel):
     product = ForeignKey("apps.Product", CASCADE)
     quantity = PositiveIntegerField()
     deadline = DateTimeField()
+    price = PositiveIntegerField()
